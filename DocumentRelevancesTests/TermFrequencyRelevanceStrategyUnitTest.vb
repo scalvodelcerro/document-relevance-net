cbd22c99523abdbdf0676b5f0f20bd5dc168aead
scalvo <scalvodelcerro@gmail.com>
2018-09-04 16:06:25 +0200

    Tests for TermFrequencyRelevanceStrategy

--- DocumentRelevancesTests/TermFrequencyRelevanceStrategyUnitTest.vb
+++ DocumentRelevancesTests/TermFrequencyRelevanceStrategyUnitTest.vb
@@ -2,11 +2,11 @@
 Imports DocumentRelevances
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
-<TestClass()> Public Class TfIdfRelevanceStrategyUnitTest
+<TestClass()> Public Class TermFrequencyRelevanceStrategyUnitTest
 
   <TestMethod()> Public Sub GivenNoTermsReturnZeroRelevanceForDocuments()
     Dim terms As IEnumerable(Of String) = {}
-    Dim tested = New TfIdfRelevanceStrategy(terms)
+    Dim tested = New TermFrequencyRelevanceStrategy(terms)
     Dim documentSummaries = New List(Of DocumentSummary) From {
       New DocumentSummary("doc1", New Dictionary(Of String, Integer)() From {
         {"aaa", 3},
@@ -19,35 +19,9 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     Assert.AreEqual(0.0, result("doc1"))
   End Sub
 
-  <TestMethod()> Public Sub GivenTermInAllDocumentsReturnZeroRelevanceForDocuments()
-    Dim repeatedTerm = "aaa"
-    Dim terms As IEnumerable(Of String) = {repeatedTerm}
-    Dim tested = New TfIdfRelevanceStrategy(terms)
-    Dim documentSummaries = New List(Of DocumentSummary) From {
-      New DocumentSummary("doc1", New Dictionary(Of String, Integer)() From {
-        {repeatedTerm, 3},
-        {"bbb", 4}
-      }, 7),
-      New DocumentSummary("doc2", New Dictionary(Of String, Integer)() From {
-        {repeatedTerm, 3},
-        {"bbb", 4}
-      }, 7),
-      New DocumentSummary("doc3", New Dictionary(Of String, Integer)() From {
-        {repeatedTerm, 3},
-        {"bbb", 4}
-      }, 7)
-    }
-
-    Dim result = tested.CalculateDocumentsRelevance(documentSummaries)
-
-    Assert.AreEqual(0.0, result("doc1"))
-    Assert.AreEqual(0.0, result("doc2"))
-    Assert.AreEqual(0.0, result("doc3"))
-  End Sub
-
   <TestMethod()> Public Sub GivenDocumentDoesNotContainTermReturnZeroRelevanceForDocument()
     Dim terms As IEnumerable(Of String) = {"aaa"}
-    Dim tested = New TfIdfRelevanceStrategy(terms)
+    Dim tested = New TermFrequencyRelevanceStrategy(terms)
     Dim documentSummaries = New List(Of DocumentSummary) From {
       New DocumentSummary("doc1", New Dictionary(Of String, Integer)() From {
         {"aaa", 3},
@@ -67,7 +41,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
   <TestMethod()> Public Sub GivenDocumentsWithSameFrequencyReturnSameRelevanceForDocuments()
     Dim terms As IEnumerable(Of String) = {"aaa", "bbb"}
-    Dim tested = New TfIdfRelevanceStrategy(terms)
+    Dim tested = New TermFrequencyRelevanceStrategy(terms)
     Dim documentSummaries = New List(Of DocumentSummary) From {
       New DocumentSummary("doc1", New Dictionary(Of String, Integer)() From {
         {"aaa", 3},
@@ -86,7 +60,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
   <TestMethod()> Public Sub GivenDocumentsWithMoreFrequencyReturnMoreRelevanceForDocument()
     Dim terms As IEnumerable(Of String) = {"aaa", "bbb"}
-    Dim tested = New TfIdfRelevanceStrategy(terms)
+    Dim tested = New TermFrequencyRelevanceStrategy(terms)
     Dim documentSummaries = New List(Of DocumentSummary) From {
       New DocumentSummary("doc1", New Dictionary(Of String, Integer)() From {
         {"aaa", 5},
@@ -101,31 +75,6 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     Dim result = tested.CalculateDocumentsRelevance(documentSummaries)
 
     Assert.IsTrue(result("doc1") > result("doc2"))
-  End Sub
-
-  <TestMethod()> Public Sub GivenTermInLessDocumentsReturnMoreRelevanceForTerm()
-    Dim terms As IEnumerable(Of String) = {"aaa", "bbb"}
-    Dim tested = New TfIdfRelevanceStrategy(terms)
-    Dim documentSummaries = New List(Of DocumentSummary) From {
-      New DocumentSummary("doc1", New Dictionary(Of String, Integer)() From {
-        {"aaa", 3},
-        {"ccc", 4}
-      }, 7),
-      New DocumentSummary("doc2", New Dictionary(Of String, Integer)() From {
-        {"bbb", 3},
-        {"ccc", 4}
-      }, 7),
-      New DocumentSummary("doc3", New Dictionary(Of String, Integer)() From {
-        {"bbb", 3},
-        {"ccc", 4}
-      }, 7)
-    }
-
-    Dim result = tested.CalculateDocumentsRelevance(documentSummaries)
-
-    Assert.IsTrue(result("doc1") > result("doc2"))
-    Assert.IsTrue(result("doc1") > result("doc3"))
-    Assert.IsTrue(result("doc2") = result("doc3"))
   End Sub
 
 End Class
