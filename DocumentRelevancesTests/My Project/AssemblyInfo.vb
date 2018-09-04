e6da1f17d40ee76669572ccc3bf6da33a3ae070b
scalvo <scalvodelcerro@gmail.com>
2018-09-04 12:59:56 +0200

    Create unit testing project

---
+++ DocumentRelevancesTests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("DocumentRelevancesTests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DocumentRelevancesTests")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("6224233f-a979-429c-9251-20aaf2a5c53b")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
